109a33283863697ebdf2271802c85608d7dcd71d
Farhan-x001 <115449922+Farhan-x001@users.noreply.github.com>
2024-02-18 12:04:45 +0530

    Updates

---
+++ precache-manifest.6296b0df3128d72dca27a8025bca90e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d46a8401a8eef8cf84a7aa34a4e7d3e",
+    "url": "/React-app/index.html"
+  },
+  {
+    "revision": "76edf0a3a313bce9d65e",
+    "url": "/React-app/static/css/2.1ba447cc.chunk.css"
+  },
+  {
+    "revision": "1bf76c0638513becc7c8",
+    "url": "/React-app/static/css/main.2e0da0b3.chunk.css"
+  },
+  {
+    "revision": "76edf0a3a313bce9d65e",
+    "url": "/React-app/static/js/2.65d54d5d.chunk.js"
+  },
+  {
+    "revision": "74e44db562299e4c6997e475287872ca",
+    "url": "/React-app/static/js/2.65d54d5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf76c0638513becc7c8",
+    "url": "/React-app/static/js/main.94193642.chunk.js"
+  },
+  {
+    "revision": "e8cf28eec96809983616",
+    "url": "/React-app/static/js/runtime-main.6236a0a5.js"
+  }
+]);
